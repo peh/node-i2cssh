17117e4a415a67a2392b7e82a285547f514911f9
Philipp Eschenbach <philipp@uberall.com>
2017-03-29 12:21:02 +0200

    fixes #3
    add -u/--user option to specify the user to connect with

--- index.js
+++ index.js
@@ -1,111 +1,119 @@
 #!/usr/bin/env node
 
-var AWS = require('aws-sdk');
-var _ = require('lodash');
-var util = require('util');
-var path = require('path');
-var file = path.resolve(__dirname, 'lib/i2cssh.js');
-var yaml = require('js-yaml');
-var fs = require('fs');
-var parseTags = require('./lib/parse-tags.js');
+var AWS = require("aws-sdk");
+var _ = require("lodash");
+var util = require("util");
+var path = require("path");
+var file = path.resolve(__dirname, "lib/i2cssh.js");
+var yaml = require("js-yaml");
+var fs = require("fs");
+var parseTags = require("./lib/parse-tags.js");
 
-var argv = require('yargs')
-  .alias('C', 'config')
-  .alias('c', 'clusters')
-  .alias('t', 'tag')
-  .alias('b', 'broadcast')
-  .argv;
+var argv = require("yargs")
+  .alias("C", "config")
+  .alias("c", "clusters")
+  .alias("t", "tag")
+  .alias("b", "broadcast")
+  .alias("u", "user").argv;
 var config = {};
 
-var I2CSSH_CONFIG_FILE = path.join(process.env.HOME, '/.i2csshrc');
+var I2CSSH_CONFIG_FILE = path.join(process.env.HOME, "/.i2csshrc");
 var hosts = argv._;
 var promises = [];
 
 function action(cmd, runConfig) {
-  cmd.unshift('osascript', '-l', 'JavaScript', file);
+  cmd.unshift("osascript", "-l", "JavaScript", file);
   cmd.push("'" + JSON.stringify(runConfig) + "'");
-  require('child_process').exec(cmd.join(" "));
+  require("child_process").exec(cmd.join(" "));
 }
 
 function checkOptionalParameters(config, runConfig) {
   if (config.broadcast === true) {
-    runConfig.broadcast = true
+    runConfig.broadcast = true;
   }
   if (config.direction !== undefined) {
-    runConfig.direction = config.direction
+    runConfig.direction = config.direction;
   }
 }
 
 function initConfig() {
   var result = {};
   if (config.aws) {
-    AWS.config.update(config.aws)
+    AWS.config.update(config.aws);
   } else {
     AWS.config.update({
-      region: 'us-west-1'
-    })
+      region: "us-west-1"
+    });
   }
   if (config.broadcast === true) {
-    result.broadcast = true
+    result.broadcast = true;
   }
-  return result
+
+  return result;
 }
 
 function parseClusters(clusters, runConfig) {
   var result = [];
-  if (typeof clusters === 'string') {
-    clusters = [clusters]
+  if (typeof clusters === "string") {
+    clusters = [clusters];
   }
   var configuredClusters = config.clusters || {};
   checkOptionalParameters(configuredClusters, runConfig);
-  return new Promise(function (resolve, reject) {
-    _.each(clusters, function (cluster) {
+  return new Promise(function(resolve, reject) {
+    _.each(clusters, function(cluster) {
       var fromConf = configuredClusters[cluster];
       if (!fromConf) {
-        console.error(cluster + " is not configured and is being ignored")
+        console.error(cluster + " is not configured and is being ignored");
       } else if (!fromConf.hosts) {
-        console.error(cluster + " has no hosts configured.")
+        console.error(cluster + " has no hosts configured.");
       } else {
         checkOptionalParameters(fromConf, runConfig);
-        result = result.concat(fromConf.hosts)
+        result = result.concat(fromConf.hosts);
       }
-      resolve(result)
-    })
+      resolve(result);
+    });
   });
 }
 
 function run() {
   var configFileLocation;
   if (argv.C) {
-    configFileLocation = argv.C
-  } else if (fs.existsSync((I2CSSH_CONFIG_FILE))) {
-    configFileLocation = I2CSSH_CONFIG_FILE
+    configFileLocation = argv.C;
+  } else if (fs.existsSync(I2CSSH_CONFIG_FILE)) {
+    configFileLocation = I2CSSH_CONFIG_FILE;
   }
 
   if (configFileLocation) {
-    config = yaml.safeLoad(fs.readFileSync(configFileLocation, 'utf8'));
+    config = yaml.safeLoad(fs.readFileSync(configFileLocation, "utf8"));
   }
 
-  initConfig()
+  initConfig();
 
   if (argv.b) {
-    config.broadcast = true
+    config.broadcast = true;
   }
 
   if (argv.t) {
-    promises.push(parseTags(argv.t, config))
+    promises.push(parseTags(argv.t, config));
   }
 
   if (argv.c) {
-    promises.push(parseClusters(argv.c, config))
+    promises.push(parseClusters(argv.c, config));
   }
 
-  Promise.all(promises).then(function (results) {
-    hosts = _.uniq(hosts.concat(_.flatten(results)));
-    action(hosts, config)
-  }, function (errors) {
-    console.error(errors)
-  })
+  if (argv.u) {
+    config.user = argv.u;
+  }
+
+  Promise.all(promises).then(
+    function(results) {
+      hosts = _.uniq(hosts.concat(_.flatten(results)));
+      action(hosts, config);
+    },
+    function(errors) {
+      console.error(errors);
+    }
+  );
 }
 
 run();
